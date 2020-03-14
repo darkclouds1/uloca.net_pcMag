bf8b467e62d77ed20ce03509024e1dd51c39fbb5
jsj <enable21@gmail.com>
2020-03-14 16:13:09 +0900

    data add

---
+++ data/uloca22.sql/uloca22_database.sql
@@ -0,0 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- 생성 시간: 20-03-14 16:10
+-- 서버 버전: 10.1.13-MariaDB
+-- PHP 버전: 7.0.0p1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `uloca22`
+--
